04118aaf5ba24eb6bca017ba612d9f769289547e
Wortman, Elliot (UG - Comp Sci & Elec Eng) <ew00710@surrey.ac.uk>
2023-11-17 11:01:04 +0000

    Favicon route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template 
+from flask import Flask, jsonify, render_template, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="./dist/assets", template_folder="./dist")
@@ -12,3 +12,8 @@ def main():
 def values():
     values = [{"N": 10}, {"P": 11}, {"K": 12}, {"T": 5}, {"H": 6}, {"M": 7}]
     return jsonify(values)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.template_folder,
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
